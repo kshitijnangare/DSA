bea3fb8c1207cffbcb08b6ff60658b6630bcffdb
kshitijnangare <kshitijnangarework@gmail.com>
2026-06-04 14:27:43 +0530

    Minimum Path Sum

---
+++ CodingNinjas/Minimum Path Sum.java
@@ -0,0 +1,74 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+    // basic recurssion
+    // public static int minSumPath(int[][] grid) {
+    //     int n = grid.length;
+    //     int m = grid[0].length;
+    //     return recursion(n-1, m-1, grid);
+    // }
+    // static int recursion(int i, int j, int[][] grid){
+    //     if(i==0 && j==0){
+    //         return grid[0][0];
+    //     }
+    //     if(i<0 || j<0){
+    //         return Integer.MAX_VALUE;
+    //     }
+    //     int up = recursion(i-1, j, grid);
+    //     int left = recursion(i, j-1, grid);
+    //     return grid[i][j] + Math.min(up,left);
+    // }
+
+    // memoization
+
+    public static int minSumPath(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n][m];
+        
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+
+        return recursion(n-1, m-1, grid, dp);
+    }
+    static int recursion(int i, int j, int[][] grid, int[][] dp){
+        if(i==0 && j==0){
+            return grid[0][0];
+        }
+        if(i<0 || j<0){
+            return Integer.MAX_VALUE;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int up = recursion(i-1, j, grid, dp);
+        int left = recursion(i, j-1, grid, dp);
+        return dp[i][j] = grid[i][j] + Math.min(up,left);
+    }
+
+    // Tabulation
+
+    // public static int minSumPath(int[][] grid) {
+    //     int n = grid.length;
+    //     int m = grid[0].length;
+
+    //     int[][] dp = new int[n][m];
+
+    //     for(int i = 0; i<n; i++){
+    //         for(int j = 0; j<m; j++){
+    //             if(i==0 && j==0){
+    //                 dp[i][j] = grid[0][0];
+    //             }else{
+    //                 int up = i>0 ? dp[i-1][j] : Integer.MAX_VALUE;
+    //                 int left = j>0 ? dp[i][j-1] : Integer.MAX_VALUE;
+    //                 dp[i][j] = grid[i][j] + Math.min(up, left);
+    //             }
+               
+    //         }
+    //     }
+    //     return dp[n-1][m-1];
+    // }
+
+}
