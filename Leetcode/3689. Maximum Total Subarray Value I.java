39edd5f45b6fda7e1f9bb6fc980ad44dbbe0c1c9
kshitijnangare <kshitijnangarework@gmail.com>
2026-06-09 21:37:40 +0530

    .

---
+++ Leetcode/3689. Maximum Total Subarray Value I.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int x: nums){
+            max = Math.max(max, x);
+            min = Math.min(min, x);
+        }
+        return (long)(max-min)*k;
+    }
+}
