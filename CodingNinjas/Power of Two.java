3183371d878ad6441d48e81120d27f135d137fbe
kshitijnangare <kshitijnangarework@gmail.com>
2026-07-25 18:15:07 +0530

    .

---
+++ CodingNinjas/Power of Two.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static boolean isPowerOfTwo(int n) {
+        // Write your code here.
+        if(n<=0){return false;}
+        if((n & (n-1)) == 0){
+            return true;
+        }
+        return false;
+    }
+}
