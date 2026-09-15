b9c0dca8faea18c4dcd58d4cc66e8ff665d8a392
kshitijnangare <kshitijnangarework@gmail.com>
2026-09-15 21:04:42 +0530

    .

---
+++ CodingNinjas/Maximum XOR of Two Numbers in an Array.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+
+class Trie{
+	Trie children[];
+	public Trie(){
+		children = new Trie[2];
+	}
+
+	public void insert(int num){
+		Trie curr = this;
+		for(int i = 31; i>=0; i--){
+			int bit = ((num>>i) & 1);
+			if(curr.children[bit]==null){
+				curr.children[bit] = new Trie();
+			}
+			curr = curr.children[bit];
+		}
+	}
+
+	public int getMaxXor(int num){
+		Trie curr = this;
+		int maxNum = 0;
+		for(int i = 31; i>=0; i--){
+			int bit = ((num>>i) & 1);
+			int flipbit = bit ^ 1;
+			if(curr.children[flipbit]!=null){
+				curr = curr.children[flipbit];
+				maxNum = maxNum | (1<<i);
+			}else{
+				curr = curr.children[bit];
+			}
+		}
+		return maxNum;
+	}
+}
+
+public class Solution {
+    public static int maximumXor(int []A) {
+        // Write your code here.
+        Trie root = new Trie();
+        for(int x: A){
+            root.insert(x);
+        }
+        int max = 0;
+        for(int x: A){
+            int currmax = root.getMaxXor(x);
+            max = Math.max(max, currmax);
+        }
+        return max;
+    }
+}
