b7c599e8fccdae82acd0dd99465b7f2b520a863c
kshitijnangare <kshitijnangarework@gmail.com>
2026-09-15 21:03:16 +0530

    .

---
+++ Leetcode/421. Maximum XOR of Two Numbers in an Array.java
@@ -0,0 +1,50 @@
+class Trie {
+	Trie children[];
+	public Trie() {
+		children = new Trie[2];
+	}
+	
+	public void insert(int num) {
+		Trie curr = this;
+		for (int i = 31; i >= 0; i--) {
+			int bit = ((num>> i) & 1);
+			if (curr.children[bit] == null) {
+				curr.children[bit] = new Trie();
+			}
+			curr = curr.children[bit];
+		}
+	}
+	
+	public int getMaxXor(int num) {
+		Trie curr = this;
+		int maxNum = 0;
+		for (int i = 31; i >= 0; i--) {
+			int bit = ((num>> i) & 1);
+			int flipbit = bit ^ 1;
+			if (curr.children[flipbit] != null) {
+				curr = curr.children[flipbit];
+				maxNum = maxNum | (1 << i);
+			} else {
+				curr = curr.children[bit];
+			}
+		}
+		return maxNum;
+	}
+}
+
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        Trie root = new Trie();
+		
+		for (int x: nums) {
+			root.insert(x);
+		}
+		
+		int max = 0;
+		for (int x: nums) {
+			int currmax = root.getMaxXor(x);
+			max = Math.max(max, currmax);
+		}
+		return max;
+    }
+}
